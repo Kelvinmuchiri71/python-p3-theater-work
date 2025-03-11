f39109da58986eb7fb097e56abcf2e3a686f2813
Kelvin Muchiri <muchirikelv71@gmail.com>
2025-03-11 06:42:46 +0300

    initial commit

--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,9 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker, backref
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
 
-Base = declarative_base(metadata=metadata)
