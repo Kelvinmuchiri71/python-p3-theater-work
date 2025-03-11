a1a0d16da182ac9e0f6255484d34436dbd9d5596
Kelvin Muchiri <muchirikelv71@gmail.com>
2025-03-11 06:45:13 +0300

    class role created

--- lib/models.py
+++ lib/models.py
@@ -12,4 +12,24 @@ engine = create_engine("sqlite:///casting.db")
 Session = sessionmaker(bind=engine)
 session = Session()
 
+class Role(Base):
+    __tablename__ = 'roles'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String, nullable=False)
+    auditions = relationship('Audition', backref=backref('role', lazy=True))
+    
+    def actors(self):
+        return [audition.actor for audition in self.auditions]
+
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+
+    def lead(self):
+        hired_actors = [audition for audition in self.auditions if audition.hired]
+        return hired_actors[0] if hired_actors else 'no actor has been hired for this role'
+    
+    def understudy(self):
+        hired_actors = [audition for audition in self.auditions if audition.hired]
+        return hired_actors[1] if len(hired_actors) > 1 else 'no actor has been hired for understudy for this role'
+
 
